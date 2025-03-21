c459f6b36b259b6bbaa16097466fd55df271b523
root <root@PC-SUPORTE-4.>
2025-03-21 16:24:08 -0300

    initial commit

---
+++ intelbrasInfo.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import requests
+from datetime import datetime
+
+# Intelbras API https://intelbras-caco-api.intelbras.com.br/
+
+def getSystemInfo(host, digest_auth):
+	url = 'http://%s/cgi-bin/magicBox.cgi?action=getSystemInfo' % host
+	res = requests.get(url, auth=digest_auth)
+	return res.text
+
+def getConfigSip(host, digest_auth):
+	url = 'http://%s/cgi-bin/configManager.cgi?action=getConfig&name=SIP' % host
+	res = requests.get(url, auth=digest_auth)
+	return res.text
+
+def getCurrentTime(host, digest_auth):
+	url = 'http://%s/cgi-bin/global.cgi?action=getCurrentTime' % host
+	res = requests.get(url, auth=digest_auth)
+	return res.text
+
+def setCurrentTime(host, digest_auth):
+	try:
+		current_datetime = datetime.today().strftime('%Y-%m-%d') + '%20' + datetime.today().strftime('%H:%M:%S')
+		url = "http://{}/cgi-bin/global.cgi?action=setCurrentTime&time={}".format(str(host), str(current_datetime),)     
+		res = requests.get(url, auth=digest_auth, stream=True, timeout=20, verify=False)
+		if res.status_code != 200:
+			raise Exception()
+		return str(res.text)
+	except Exception:
+		raise Exception("ERROR - During Set Current Time")
+
+def main():
+	user = 'admin'
+	pwd = '***REMOVED***'
+	digest_auth = requests.auth.HTTPDigestAuth(user, pwd)
+
+	host = input("<IP:porta>: ").strip().lower()
+
+	info = getSystemInfo(host, digest_auth)
+	sip = getConfigSip(host, digest_auth)
+	datetime = getCurrentTime(host, digest_auth)
+
+	print('--------------------------')
+	print(info)
+	print('--------------------------')
+	print(sip)
+	print('--------------------------')
+	print(datetime)
+	print('--------------------------')
+
+	ask_to_update_time = str(input('Atualizar dia/hora? (S/N) '))
+	while True:
+		if ask_to_update_time.upper() == 'S':
+			setCurrentTime(host, digest_auth)
+			datetime = getCurrentTime(host, digest_auth)
+			print('--------------------------')
+			print(datetime)
+			print('--------------------------')
+			break
+		elif ask_to_update_time.upper() == 'N':
+			break
+		else:
+			ask_to_update_time = str(input('Atualizar dia/hora? (S/N) '))
+
+if __name__ == '__main__':
+	main()
