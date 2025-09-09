698c57d9f6fbe1af0e92301ad4478bab532f51e9
Felipe <felipemartabatista@gmail.com>
2025-09-09 17:43:51 -0300

    option menu created to select the functions

--- intelbrasInfo.py
+++ intelbrasInfo.py
@@ -32,36 +32,48 @@ def setCurrentTime(host, digest_auth):
 	except Exception:
 		raise Exception("ERROR - During Set Current Time")
 
+def reboot(host, digest_auth):
+	url = 'http://%s/cgi-bin/magicBox.cgi?action=reboot' % host
+	res = requests.get(url, auth=digest_auth)
+	return res.text + '\n'
+
 def main():
 	digest_auth = requests.auth.HTTPDigestAuth(USER, PASSWORD)
-
 	host = input("<IP:porta>: ").strip().lower()
 
-	info = getSystemInfo(host, digest_auth)
-	sip = getConfigSip(host, digest_auth)
-	datetime = getCurrentTime(host, digest_auth)
-
-	print('--------------------------')
-	print(info)
-	print('--------------------------')
-	print(sip)
-	print('--------------------------')
-	print(datetime)
-	print('--------------------------')
-
-	ask_to_update_time = str(input('Atualizar dia/hora? (S/N) '))
 	while True:
-		if ask_to_update_time.upper() == 'S':
+		print(
+			"---------------------------------\n"+
+			"HOST: " + host + "\n" +	
+			"---------------------------------\n"+
+			"| 1. Informações gerais 	|\n"+
+			"| 2. Informações sip 		|\n"+
+			"| 3. Data e hora 		|\n"+
+			"| 4. Ajustar data e hora 	|\n"+
+			"| 5. Reiniciar 			|\n"+
+			"| 0. Sair 			|\n"+
+			"---------------------------------\n")
+		opt = input("Digite: ").strip()
+		if opt == "0":
+			break
+		elif opt == "1":
+			info = getSystemInfo(host, digest_auth)
+			print(info)
+		elif opt == "2":
+			sip = getConfigSip(host, digest_auth)
+			print(sip)
+		elif opt == "3":
+			datetime = getCurrentTime(host, digest_auth)
+			print(datetime)
+		elif opt == "4":
 			setCurrentTime(host, digest_auth)
 			datetime = getCurrentTime(host, digest_auth)
-			print('--------------------------')
 			print(datetime)
-			print('--------------------------')
-			break
-		elif ask_to_update_time.upper() == 'N':
-			break
+		elif opt == "5":
+			res = reboot(host, digest_auth)
+			print(res)
 		else:
-			ask_to_update_time = str(input('Atualizar dia/hora? (S/N) '))
+			print("\nOpção inválida!\n")
 
 if __name__ == '__main__':
 	main()
